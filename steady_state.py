f2ebfd15f2f0773fc2dacaf895779c1feced4013
Your Name <you@example.com>
2022-10-13 08:23:42 +0200

    Added prints

--- steady_state.py
+++ steady_state.py
@@ -118,177 +118,41 @@ def find_ss(model,do_print=False): # add other inputs
 
     # print results
     if do_print:
-        print(f' K_ss = {res.x[0]:8.4f}')
-        print(f' L_ss = {res.x[1]:8.4f}')
+        print('Solver terminated')
+        # print(f' K_ss = {res.x[0]:8.4f}')
+        # print(f' L_ss = {res.x[1]:8.4f}')
 
     # Simulate the model at the found steady state once
+    # Print steady state aggregates and prices automatically (also consumption, bonds, investment, income and government spending, tax income)
 
+    # # Run steady state
 
+    obj_ss(res.x,model,do_print=False)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def find_ss_old(model,method='direct',do_print=False,K_min=1.0,K_max=4.0,NK=10):
-    """ find steady state using the direct or indirect method """
-
-    t0 = time.time()
-
-    if method == 'direct':
-        find_ss_direct(model,do_print=do_print,K_min=K_min,K_max=K_max,NK=NK)
-    elif method == 'indirect':
-        find_ss_indirect(model,do_print=do_print)
-    else:
-        raise NotImplementedError
-
-    if do_print: print(f'found steady state in {elapsed(t0)}')
-
-def find_ss_direct(model,do_print=False,K_min=1.0,K_max=10.0,NK=10):
-    """ find steady state using direct method """
-
-    # a. broad search
-    if do_print: print(f'### step 1: broad search ###\n')
-
-    K_ss_vec = np.linspace(K_min,K_max,NK) # trial values
-    clearing_A = np.zeros(K_ss_vec.size) # asset market errors
-
-    for i,K_ss in enumerate(K_ss_vec):
-        
-        try:
-            clearing_A[i] = obj_ss(K_ss,model,do_print=do_print)
-        except Exception as e:
-            clearing_A[i] = np.nan
-            print(f'{e}')
-            
-        if do_print: print(f'clearing_A = {clearing_A[i]:12.8f}\n')
-            
-    # b. determine search bracket
-    if do_print: print(f'### step 2: determine search bracket ###\n')
-
-    K_max = np.min(K_ss_vec[clearing_A < 0])
-    K_min = np.max(K_ss_vec[clearing_A > 0])
-
-    if do_print: print(f'K in [{K_min:12.8f},{K_max:12.8f}]\n')
-
-    # c. search
-    if do_print: print(f'### step 3: search ###\n')
-
-    root_finding.brentq(
-        obj_ss,K_min,K_max,args=(model,),do_print=do_print,
-        varname='K_ss',funcname='A_hh-K'
-    )
-
-def find_ss_indirect(model,do_print=False):
-    """ find steady state using indirect method """
-
-    par = model.par
-    ss = model.ss
-
-    # a. exogenous and targets
-    ss.L = 1.0
-    ss.r = par.r_ss_target
-    ss.w = par.w_ss_target
-
-    assert (1+ss.r)*par.beta < 1.0, '(1+r)*beta < 1, otherwise problems might arise'
-
-    # b. stock and capital stock from household behavior
-    model.solve_hh_ss(do_print=do_print) # give us ss.a and ss.c (steady state policy functions)
-    model.simulate_hh_ss(do_print=do_print) # give us ss.D (steady state distribution)
-    if do_print: print('')
-
-    ss.K = ss.A_hh = np.sum(ss.a*ss.D)
-    
-    # c. back technology and depreciation rate
-    ss.Gamma = ss.w / ((1-par.alpha)*(ss.K/ss.L)**par.alpha)
-    ss.rk = par.alpha*ss.Gamma*(ss.K/ss.L)**(par.alpha-1)
-    par.delta = ss.rk - ss.r
-
-    # d. remaining
-    ss.Y = ss.Gamma*ss.K**par.alpha*ss.L**(1-par.alpha)
-    ss.C = ss.Y - par.delta*ss.K
-    ss.C_hh = np.sum(ss.D*ss.c)
-
-    # e. print
     if do_print:
 
-        print(f'Implied K = {ss.K:6.3f}')
-        print(f'Implied Y = {ss.Y:6.3f}')
-        print(f'Implied Gamma = {ss.Gamma:6.3f}')
-        print(f'Implied delta = {par.delta:6.3f}') # check is positive
-        print(f'Implied K/Y = {ss.K/ss.Y:6.3f}') 
-        print(f'Discrepancy in K-A_hh = {ss.K-ss.A_hh:12.8f}') # = 0 by construction
-        print(f'Discrepancy in C-C_hh = {ss.C-ss.C_hh:12.8f}\n') # != 0 due to numerical error 
+        ss = model.ss
+        print('')
+        print('Steady state aggregates:')
+        print(f' K_ss = {ss.K:8.4f}')
+        print(f' L_ss = {ss.L:8.4f}')
+        print(f' Y_ss = {ss.Y:8.4f}')
+        print(f' G_ss = {ss.G:8.4f}')
+        print(f' B_ss = {ss.B:8.4f}')
+        print(f' I_ss = {ss.I:8.4f}')
+        print(f' C_ss = {ss.C:8.4f}')
+        print('')
 
-def obj_ss_alt(r_ss,model,do_print=False):
-    """ objective when solving for steady state capital """
+        print('steady state prices')
+        print(f' w_ss = {ss.w:8.4f}')
+        print(f' r_ss = {ss.r:8.4f}')
+        print('')
 
-    par = model.par
-    ss = model.ss
+        print('Check for market clearing')
+        print(f'Excess savings           ={ss.clearing_A:8.4f}')
+        print(f'Exess consumption demand ={ss.clearing_C:8.4f}')
+        print(f'Excess labour supply     ={ss.clearing_L:8.4f}')        
 
-    # a. set real interest rate and rental rate
-    ss.r = r_ss
-    ss.rk = ss.r+par.delta
 
-    # b. production
-    ss.Gamma = par.Gamma_ss # model user choice
-    ss.K = (ss.rk/par.alpha*ss.Gamma)**(1/(par.alpha-1))
-    ss.L = 1.0 # by assumption
-    ss.Y = ss.Gamma*ss.K**par.alpha*ss.L**(1-par.alpha)    
-
-    # b. implied wage
-    ss.w = (1.0-par.alpha)*ss.Gamma*(ss.K/ss.L)**par.alpha
-
-    # c. household behavior
-    if do_print:
-
-        print(f'guess {ss.K = :.4f}')    
-        print(f'implied {ss.r = :.4f}')
-        print(f'implied {ss.w = :.4f}')
-
-    model.solve_hh_ss(do_print=do_print)
-    model.simulate_hh_ss(do_print=do_print)
-
-    ss.A_hh = np.sum(ss.a*ss.D) # hint: is actually computed automatically
-    ss.C_hh = np.sum(ss.c*ss.D)
-
-    if do_print: print(f'implied {ss.A_hh = :.4f}')
-
-    # d. market clearing
-    ss.clearing_A = ss.A_hh-ss.K
-
-    ss.I = ss.K - (1-par.delta)*ss.K
-    ss.C = ss.Y - ss.I
-    ss.clearing_C = ss.C_hh-ss.C
-
-    return ss.clearing_A # target to hit
-
+    # model.solve_hh_ss(do_print=do_print)
+    # model.simulate_hh_ss(do_print=do_print)
